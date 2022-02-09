7f0cb9b2d34441a1e6351b937ab023ad37713f97
Panova Natalia <pannat.job@gmail.com>
2022-02-09 21:31:17 +0300

    Step 3. Bounce of the wall

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
+const COLOR = '#a985c9';
+const RADIUS_BALL = 10;
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-const dx = 2;
-const dy = -2;
+let dx = 2;
+let dy = -2;
 
 const drawBall = () => {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2)
-    ctx.fillStyle = '#0095DD';
+    ctx.arc(x, y, RADIUS_BALL, 0, Math.PI*2)
+    ctx.fillStyle = COLOR;
     ctx.fill();
     ctx.closePath();
 }
@@ -19,8 +21,19 @@ const drawBall = () => {
 const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    x+=dx;
-    y+=dy;
+
+    if (y + dy > canvas.height - RADIUS_BALL || y + dy < RADIUS_BALL) {
+        dy = -dy;
+    }
+
+    if (x + dx > canvas.width - RADIUS_BALL || x + dx < RADIUS_BALL) {
+        dx = -dx;
+    }
+
+    x += dx;
+    y += dy;
 }
+
+draw()
 
 setInterval(draw, 10);
