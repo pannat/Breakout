3e3b5c18d7784c43920e67d941e659e262890577
Panova Natalia <pannat.job@gmail.com>
2022-02-09 22:05:04 +0300

    Step 4. Paddle and keyboard controls

--- src/index.js
+++ src/index.js
@@ -10,6 +10,38 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleY = canvas.height - paddleHeight;
+
+let rightPressed = false;
+let leftPressed = false;
+
+const pressKeyDown = (evt) => {
+    if (evt.key === 'Right' || evt.key === 'ArrowRight') {
+        rightPressed = true;
+    } else if (evt.key === 'Left' || evt.key === 'ArrowLeft') {
+        leftPressed = true;
+    }
+}
+
+const pressKeyUp = (evt) => {
+    if (evt.key === 'Right' || evt.key === 'ArrowRight') {
+        rightPressed = false;
+    } else if (evt.key === 'Left' || evt.key === 'ArrowLeft') {
+        leftPressed = false;
+    }
+}
+
+const drawPaddle = () => {
+    ctx.beginPath();
+    ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
+    ctx.fillStyle = COLOR;
+    ctx.fill();
+    ctx.closePath();
+}
+
 const drawBall = () => {
     ctx.beginPath();
     ctx.arc(x, y, RADIUS_BALL, 0, Math.PI*2)
@@ -21,6 +53,7 @@ const drawBall = () => {
 const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
 
     if (y + dy > canvas.height - RADIUS_BALL || y + dy < RADIUS_BALL) {
         dy = -dy;
@@ -32,8 +65,25 @@ const draw = () => {
 
     x += dx;
     y += dy;
+
+    if (rightPressed) {
+        paddleX += 7;
+
+        if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }
+
+    } else if (leftPressed) {
+        paddleX -= 7;
+
+        if (paddleX < 0) {
+            paddleX = 0;
+        }
+    }
 }
 
-draw()
+
+document.addEventListener('keydown', pressKeyDown, false);
+document.addEventListener('keyup', pressKeyUp, false);
 
 setInterval(draw, 10);
